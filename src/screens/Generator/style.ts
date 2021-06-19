0a1701a4fd4f5b9f6b89ac3cb5bd58c7eec96fea
Gui-dev <adrianon10@gmail.com>
2021-06-19 03:26:20 -0300

    chorus: add style screen Generator

--- src/screens/Generator/style.ts
+++ src/screens/Generator/style.ts
@@ -1,7 +1,59 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-  display: flex;
+  flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: #222;
+`
+
+export const Image = styled.Image`
+  margin-bottom: 30px;
+`
+
+export const Characters = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  color: #FFF;
+`
+
+export const SliderContainer = styled.View`
+  width: 90%;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 8px;
+`
+
+export const ButtonGenerate = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.9
+})`
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  width: 90%;
+  background-color: #D4A13F;
+  border-radius: 8px;
+`
+
+export const ButtonGenerateText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #FFF;
+`
+
+export const ResultGenerate = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  padding: 20px;
+  background-color: #3FD4C9;
+  border-radius: 8px;
+`
+
+export const ResultGenerateText = styled.Text`
+  font-size: 20px;
+  color: #333;
 `
