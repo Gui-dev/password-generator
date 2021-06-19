0d20f16ff85259cf76c1cd09b85c637c9afc7322
Gui-dev <adrianon10@gmail.com>
2021-06-19 03:27:07 -0300

    feat: add function handleGeneratePassword

--- src/screens/Generator/index.tsx
+++ src/screens/Generator/index.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import Slider from '@react-native-community/slider'
 
-import { Container } from './style'
+import logo from './../../assets/logo.png'
+import { Container, Image, Characters, SliderContainer, ButtonGenerate, ButtonGenerateText,
+  ResultGenerate, ResultGenerateText
+} from './style'
 
 export const Generator: React.FC = () => {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$*'
+  const [slideValue, setSlideValue] = useState<number>(6)
+  const [password, setPassword] = useState<string>()
+
+  const handleGeneratePassword = () => {
+    let pass = ''
+
+    for (let i = 0, n = characters.length; i < slideValue; i++) {
+      pass += characters.charAt(Math.floor(Math.random() * n))
+    }
+
+    setPassword(pass)
+  }
+
   return (
     <Container>
-      <Text>Generator</Text>
+      <Image source={ logo }/>
+
+      <Characters>{ slideValue?.toFixed(0) } Caracteres</Characters>
+
+      <SliderContainer>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={6}
+          maximumValue={15}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackTintColor="#000"
+          value={ slideValue }
+          onValueChange={ setSlideValue }
+        />
+      </SliderContainer>
+
+      <ButtonGenerate onPress={ handleGeneratePassword }>
+        <ButtonGenerateText>Gerar senha</ButtonGenerateText>
+      </ButtonGenerate>
+
+      { password && (
+        <ResultGenerate>
+          <ResultGenerateText>{ password }</ResultGenerateText>
+        </ResultGenerate>
+      ) }
     </Container>
   )
 }
